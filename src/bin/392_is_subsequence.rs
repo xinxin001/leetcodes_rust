30e50e305ab08eb46f523683b78a045e3a900575
Li Xin Rui <li.xinrui3@gmail.com>
2023-06-17 21:11:57 -0700

    boop

--- src/bin/392_is_subsequence.rs
+++ src/bin/392_is_subsequence.rs
@@ -12,6 +12,37 @@ pub fn is_subsequence(s: String, t: String) -> bool {
     !s_iter.peek().is_some()
 }
 
+pub fn alt_is_subsequence(s: String, t: String) -> bool {
+    let mut siter = s.chars().peekable();
+    for c in t.chars() {
+        if let Some(sc) = siter.peek() {
+            if &c == sc {
+                siter.next();
+            }
+        } else {
+            return true;
+        }
+    }
+    !siter.peek().is_some()
+}
+
+pub fn vec_is_subsequence(s: String, t: String) -> bool {
+    let schars = s.as_bytes();
+    if schars.len() == 0 {
+        return true;
+    }
+    let mut idx = 0;
+    for c in t.bytes() {
+        if c == schars[idx] {
+            idx += 1;
+        }
+        if idx == schars.len() {
+            return true;
+        }
+    }
+    return false;
+}
+
 fn main() {
     let s = "abc".to_string();
     let t = "ahbgdc".to_string();
